09f0694dfc4dd4dbcac65da79994961cb760ab29
William del Rosario <williamdelrosario@yahoo.com>
2019-06-22 01:26:15 +0200

    Behat initialized

---
+++ src/AppBundle/Controller/ProductAdminController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Product;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class ProductAdminController extends Controller
+{
+    /**
+     * @Route("/admin/products", name="product_list")
+     */
+    public function listAction()
+    {
+        $products = $this->getDoctrine()
+            ->getRepository('AppBundle:Product')
+            ->findAll();
+
+        return $this->render('product/list.html.twig', [
+            'products' => $products
+        ]);
+    }
+}
